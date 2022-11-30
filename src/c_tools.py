9aa5d331c1017e9371d731693b0a852b2dcba4be
Benjamin Miramontes <benjamin@miramontes.dev>
2022-11-29 21:51:13 -0600

    Added synchronized update to compilation

--- src/c_tools.py
+++ src/c_tools.py
@@ -1,4 +1,6 @@
+import asyncio
 import subprocess
+from functools import partial
 from pathlib import Path
 from utils import random_id
 from typing import Tuple
@@ -7,13 +9,23 @@ from typing import Tuple
 GCC_ARGS = []
 
 
-def compile(c_contents: str) -> Tuple[bool, bytes, Path]:
+async def async_subprocess(*args):
+    loop = asyncio.get_running_loop()
+    return await loop.run_in_executor(None, partial(
+        subprocess.check_output,
+        args,
+        stderr=subprocess.STDOUT,
+        timeout=5,
+    ))
+
+
+async def compile(c_contents: str) -> Tuple[bool, bytes, Path]:
     exe_path = Path("/data/" + random_id())
     source_path = exe_path.with_suffix(".c")
     source_path.write_text(c_contents)
 
     try:
-        output = subprocess.check_output(["gcc", source_path, "-o", exe_path, *GCC_ARGS], stderr=subprocess.STDOUT, timeout=5)
+        output = await async_subprocess("gcc", source_path, "-o", exe_path, *GCC_ARGS)
         return True, output, exe_path
     except subprocess.CalledProcessError as error:
         return False, error.output, exe_path
@@ -21,9 +33,9 @@ def compile(c_contents: str) -> Tuple[bool, bytes, Path]:
         return False, b"Compilation killed by timeout!", exe_path
 
 
-def run(exe_path: Path) -> Tuple[bool, bytes]:
+async def run(exe_path: Path) -> Tuple[bool, bytes]:
     try:
-        output = subprocess.check_output([str(exe_path)], stderr=subprocess.STDOUT, timeout=5)
+        output = await async_subprocess(str(exe_path))
         return True, output
     except subprocess.CalledProcessError as error:
         return False, error.output
--- src/main.py
+++ src/main.py
@@ -4,16 +4,20 @@ import c_tools
 import os
 import statics
 import hashlib
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, WebSocket
 from fastapi.responses import JSONResponse, HTMLResponse
 from pydantic import BaseModel
 from database import db
 from utils import random_id
 from hmac import compare_digest
+from typing import Dict, Set
 
 
 BASE_URL = os.environ.get("BASE_URL", "")
 ADMIN_HASH = hashlib.sha256(os.environ.get("ADMIN_TOKEN", "").encode()).digest()
+
+
+DOCUMENT_SUBSCRIBERS: Dict[str, Set[WebSocket]] = {}
 
 
 app = FastAPI()
@@ -83,12 +87,35 @@ async def run_c(request: CRunRequest):
     if request.document_id not in db.documents.c:
         raise JsonError("Invalid Document ID")
 
-    success, output, exe_path = c_tools.compile(request.contents)
-    if not success:
-        return {"status": "success", "compile": False, "run": False, "output": output}
+    if request.document_id not in DOCUMENT_SUBSCRIBERS:
+        DOCUMENT_SUBSCRIBERS[request.document_id] = set()
 
-    success, output = c_tools.run(exe_path)
-    return {"status": "success", "compile": True, "run": success, "output": output}
+    compile_success, output, exe_path = await c_tools.compile(request.contents)
+    if compile_success:
+        run_success, output = await c_tools.run(exe_path)
+        response = {"status": "success", "compile": True, "run": run_success, "output": output}
+    else:
+        response = {"status": "success", "compile": False, "run": False, "output": output}
+
+    for websocket in tuple(DOCUMENT_SUBSCRIBERS[request.document_id]):
+        try:
+            await websocket.send_json(response)
+        except:
+            DOCUMENT_SUBSCRIBERS[request.document_id].discard(websocket)
+    return response
+
+
+@app.websocket("/subscribe/c/{document_id}")
+async def subscribe_c(websocket: WebSocket, document_id: str):
+    await websocket.accept()
+    if document_id not in DOCUMENT_SUBSCRIBERS:
+        DOCUMENT_SUBSCRIBERS[document_id] = set()
+    DOCUMENT_SUBSCRIBERS[document_id].add(websocket)
+    try:
+        while True:
+            await websocket.receive_text()
+    except:
+        DOCUMENT_SUBSCRIBERS[document_id].discard(websocket)
 
 
 @app.on_event("shutdown")
